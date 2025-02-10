81691f981010b3fcc3fb84a03953539a5ae5fac3
7asanka <hassan2002_kimo@hotmail.com>
2025-02-10 11:30:24 -0800

    0x15 task 1 fix

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -14,23 +14,16 @@ if __name__ == "__main__":
 
     url = "https://jsonplaceholder.typicode.com"
 
-    user_info = requests.get("{}/users/{}".format(url, employee_id))
-    employee_data = user_info.json()
-    employee_name = employee_data.get("name")
+    user_response = requests.get(f"{url}/users/{employee_id}")
+    user_data = user_response.json()
+    username = user_data.get("username")
 
-    todos_info = requests.get("{}/todos?userId={}".format(
-        url, employee_id))
-    todos = todos_info.json()
+    todos_response = requests.get(f"{url}/todos?userId={employee_id}")
+    todos = todos_response.json()
 
-    completed_tasks = [todo.get("title") for todo in todos
-                       if todo.get("completed") is True]
-    tasks_len = len(todos)
-    completed_len = len(completed_tasks)
+    csv_filename = f"{employee_id}.csv"
 
-    filename = "{}.csv".format(employee_id)
-
-    with open(filename, mode='w', newline='') as file:
+    with open(csv_filename, mode='w', newline='') as file:
         writer = csv.writer(file, quoting=csv.QUOTE_ALL)
         for task in todos:
-            writer.writerow([employee_id, employee_name,
-                            task["completed"], task["title"]])
+            writer.writerow([employee_id, username, task["completed"], task["title"]])
