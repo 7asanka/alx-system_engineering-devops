fdf4a844a35217115c6cf5485b2fab5ada75fc86
7asanka <hassan2002_kimo@hotmail.com>
2025-02-27 12:41:16 -0800

    0x18 task 0

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,7 @@
 # Fix why Apache returns 500 error, then automate using Puppet
 
-exec { '/var/www/html/wp-setting.php':
-  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
-  command => "sed -i 's/.phpp/.php/g' /var/www/html/wp-settings.php",
+$file_to_edit = '/var/www/html/wp-settings.php'
+exec { 'replace_line':
+  command => "sed -i 's/phpp/php/g' ${file_to_edit}",
+  path    => ['/bin','/usr/bin']
 }
