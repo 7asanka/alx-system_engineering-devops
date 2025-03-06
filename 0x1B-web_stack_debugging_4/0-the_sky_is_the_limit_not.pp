e13b5e6c0f1460d75a6a304d5d50b3028429b2f2
7asanka <hassan2002_kimo@hotmail.com>
2025-03-06 07:54:57 -0800

    0xB task 0 added;

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,11 @@
+# Adjust the amount of traffic the server can handle
+
+exec { 'fix--for-nginx':
+  command => 'sed -i "s/15/4096/" /etc/default/nginx',
+  path    => '/usr/local/bin/:/bin'
+}
+
+exec { 'nginx-restart':
+  command => 'nginx restart',
+  path    => '/etc/init.d/'
+}
